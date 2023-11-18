96968054595980f58fa6b34da25b9d74055f6068
pask0n <phatsakon2550@gmail.com>
2023-11-18 22:36:52 +0700

    Add gam()e functionand related changes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3)
-    console.log(random)
 
     if (random === 0) {
         return 'rock'
@@ -13,40 +12,80 @@ function getComputerChoice() {
     } 
 }
 
-let playerChoice = prompt('Rock, paper, or scissors?').toLowerCase()
-let computerChoice = getComputerChoice()
+let playerChoice;
+let computerChoice;
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerChoice, computerChoice) {
+    playerChoice = prompt('Rock, paper, or scissors?').toLowerCase()
+    computerChoice = getComputerChoice()
+
     if (playerChoice === computerChoice) {
         alert('It\'s a draw! Same choice from both players')
     } else if (
         playerChoice === 'rock'
         && computerChoice === 'paper') {
             alert('Your lose! Paper beats Rock')
+            computerScore += 1
     } else if (
         playerChoice === 'rock'
         && computerChoice === 'scissors') {
             alert('You win! Rock beats Scissors')
+            playerScore += 1
     } else if (
         playerChoice === 'paper'
         && computerChoice === 'rock') {
             alert('You win! Paper beats Rock')
+            playerScore += 1
     } else if (
         playerChoice === 'paper'
         && computerChoice === 'scissors') {
             alert('You lose! Scissors beats paper')
+            computerScore += 1
     } else if  (
         playerChoice === 'scissors'
         && computerChoice === 'rock') {
             alert('You lose! Rock beats scissors')
+            computerScore += 1
     } else if (
         playerChoice === 'scissors'
         && computerChoice === 'paper') {
             alert('You win! Scissors beats paper')
-        }
+            playerScore += 1
+    } else {
+        alert('Invalid choice. Counts as a loss')
+        computerScore += 1
+    }
 }
 
 console.log('computerChoice = ' + computerChoice)
 console.log('playerChoice = ' + playerChoice)
 
-playRound(playerChoice, computerChoice);
+function game() {
+    console.log('game started')
+    for (let i = 0; i < 5; i++) {
+        playRound(playerChoice, computerChoice);
+        playerChoice = "";
+        computerChoice = "";
+    }
+
+    let finalResult;
+    if (playerScore > computerScore) {
+        finalResult = 'You win!'
+    } else if (computerScore > playerScore) {
+        finalResult = 'You lose!'
+    } else if (playerScore === playerScore) {
+        finalResult = 'It\'s a tie!'
+    } else {
+        return
+    }
+    
+    alert(`${finalResult}
+    Player ${playerScore}-${computerScore} Computer`)
+    console.log(`${finalResult}
+    Player ${playerScore}-${computerScore} Computer`)
+}
+
+game();
