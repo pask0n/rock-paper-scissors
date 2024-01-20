7d7b5d817fa87cfcd99283dd71cf14b91d3f63c4
pask0n <phatsakon2550@gmail.com>
2024-01-20 22:28:58 +0700

    rps-ui Version 1, No CSS

--- script.js
+++ script.js
@@ -2,61 +2,120 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3)
 
     if (random === 0) {
-        return 'rock'
+        return 'Rock'
     } else if (random === 1) {
-        return 'paper'
+        return 'Paper'
     } else if (random === 2) {
-        return 'scissors'
+        return 'Scissors'
     } else {
         return
     } 
 }
 
-let playerChoice;
-let computerChoice;
+const playerScDis = document.querySelector('#player-score-dis')
+const computerScDis = document.querySelector('#computer-score-dis')
+
+const finalResult = document.querySelector('#final-result')
+
+const btnRock = document.querySelector('#btn-rock');
+const btnPaper = document.querySelector('#btn-paper');
+const btnScissors = document.querySelector('#btn-scissors');
+
+const roundMovePlayer = document.querySelector('#round-move-player')
+const roundMoveComputer = document.querySelector('#round-move-computer')
+const roundResult = document.querySelector('#round-result')
+
+btnRock.addEventListener('click', () => {
+    playRound('Rock',getComputerChoice());
+    updateScoreDis();
+})
+btnPaper.addEventListener('click', () => {
+    playRound('Paper',getComputerChoice());
+    updateScoreDis();
+})
+btnScissors.addEventListener('click', () => {
+    playRound('Scissors',getComputerChoice());
+    updateScoreDis();
+})
 
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound(playerChoice, computerChoice) {
-    playerChoice = prompt('Rock, paper, or scissors?').toLowerCase()
-    computerChoice = getComputerChoice()
-
     if (playerChoice === computerChoice) {
-        alert('It\'s a draw! Same choice from both players')
+        displayRoundResultDraw(playerChoice, computerChoice)
     } else if (
-        playerChoice === 'rock'
-        && computerChoice === 'paper') {
-            alert('Your lose! Paper beats Rock')
+        playerChoice === 'Rock'
+        && computerChoice === 'Paper') {
+            displayRoundResultLose(playerChoice, computerChoice)
             computerScore += 1
     } else if (
-        playerChoice === 'rock'
-        && computerChoice === 'scissors') {
-            alert('You win! Rock beats Scissors')
+        playerChoice === 'Rock'
+        && computerChoice === 'Scissors') {
+            displayRoundResultWin(playerChoice, computerChoice)
             playerScore += 1
     } else if (
-        playerChoice === 'paper'
-        && computerChoice === 'rock') {
-            alert('You win! Paper beats Rock')
+        playerChoice === 'Paper'
+        && computerChoice === 'Rock') {  
+            displayRoundResultWin(playerChoice, computerChoice)
             playerScore += 1
     } else if (
-        playerChoice === 'paper'
-        && computerChoice === 'scissors') {
-            alert('You lose! Scissors beats paper')
-            computerScore += 1
-    } else if  (
-        playerChoice === 'scissors'
-        && computerChoice === 'rock') {
-            alert('You lose! Rock beats scissors')
+        playerChoice === 'Paper'
+        && computerChoice === 'Scissors') {
+            displayRoundResultLose(playerChoice, computerChoice)
             computerScore += 1
     } else if (
-        playerChoice === 'scissors'
-        && computerChoice === 'paper') {
-            alert('You win! Scissors beats paper')
+        playerChoice === 'Scissors'
+        && computerChoice === 'Rock') {
+            displayRoundResultLose(playerChoice, computerChoice)
+            computerScore += 1
+    } else if (
+        playerChoice === 'Scissors'
+        && computerChoice === 'Paper') {
+            displayRoundResultWin(playerChoice, computerChoice)
             playerScore += 1
     } else {
         alert('Invalid choice. Counts as a loss')
         computerScore += 1
+    }
+}
+
+function displayRoundResultWin(playerChoice, computerChoice) {
+    roundMovePlayer.textContent = `Your Move : ${playerChoice}`
+    roundMoveComputer.textContent = `Computer's Move : ${computerChoice}`
+    roundResult.textContent = 'You Win!'
+}
+
+function displayRoundResultLose(playerChoice, computerChoice) {
+    roundMovePlayer.textContent = `Your Move : ${playerChoice}`
+    roundMoveComputer.textContent = `Computer's Move : ${computerChoice}`
+    roundResult.textContent = 'You Lose!'
+}
+
+function displayRoundResultDraw(playerChoice, computerChoice) {
+    roundMovePlayer.textContent = `Your Move : ${playerChoice}`
+    roundMoveComputer.textContent = `Computer's Move : ${computerChoice}`
+    roundResult.textContent = 'It\'s a draw!'
+}
+
+function updateScoreDis() {
+    playerScDis.textContent = playerScore;
+    computerScDis.textContent = computerScore;
+    displayGameResult()
+}
+
+function displayGameResult() {
+    finalResult.textContent = ''
+    if (playerScore == 3) {
+        finalResult.textContent = 'You Win!'
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore == 3) {
+        finalResult.textContent = 'You Lose, The Computer Wins!'
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        return
     }
 }
 
@@ -83,4 +142,3 @@ function game() {
     Player ${playerScore}-${computerScore} Computer`)
 }
 
-game();
